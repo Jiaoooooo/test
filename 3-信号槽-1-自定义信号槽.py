4c8757d450d60df91bd9fda91124d88be718a231
Jiaoooooo <Jiao@jiaozhiqiangdeMac-mini.local>
2018-01-16 09:31:10 +0800

    GitHub test
    测试github上传 克隆的操作

---
+++ 3-信号槽-1-自定义信号槽.py
@@ -0,0 +1,31 @@
+from PyQt5.QtCore import QObject, pyqtSignal
+
+class QTypeSignal(QObject):
+    sendMsg = pyqtSignal(object) #object是什么鬼?
+
+    def __init__(self):
+        super(QTypeSignal, self).__init__()
+
+    def run(self):
+        self.sendMsg.emit('Hello, World!') #emit是发射的意思?
+
+class QTypeSlot(QObject):
+    def __init__(self):
+        super(QTypeSlot, self).__init__()
+
+    def get(self, msg):
+        print('接收到信号=>'+ msg)
+
+if __name__ == '__main__':
+    send = QTypeSignal()
+    slot = QTypeSlot()
+
+    print('-------把信号绑定到槽上------')
+    send.sendMsg.connect(slot.get)
+
+    send.run()
+
+    print('-------吧信号与槽函数断开-----')
+    send.sendMsg.disconnect(slot.get)
+    send.run()
+
